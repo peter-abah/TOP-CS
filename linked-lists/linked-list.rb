c897e078710581dbc7d390f8df96566f8565cccc
Peter Abah <peterabah.ob@gmail.com>
2021-05-15 08:07:51 +0100

    create LinkedList class

---
+++ linked-lists/linked-list.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+# A class implementing Linked list data structure
+class LinkedList
+  attr_reader :head
+
+  def initialize(head = nil)
+    @head = head
+  end
+end
